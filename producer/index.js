5e0fc66f8e46563f8d942cfff8a7fd927e67c527
Kata <katarina.zec@myy.haaga-helia.fi>
2023-10-22 12:07:21 +0300

    small fixes

--- producer/index.js
+++ producer/index.js
@@ -20,43 +20,43 @@ const run = async () => {
         queueMessage();
     }, 2500)
 }
-    run().catch(console.error);
+run().catch(console.error);
 
 
-    const idNumbers = [
-        "311299-999X",
-        "010703A999Y",
-        "240588+9999",
-        "NNN588+9999",
-        "112233-9999",
-        "300233-9999",
-        "30233-9999"
-    ]
+const idNumbers = [
+    "311299-999X",
+    "010703A999Y",
+    "240588+9999",
+    "NNN588+9999",
+    "112233-9999",
+    "300233-9999",
+    "30233-9999"
+]
 
-    function randomizeIntegerBetween(from, to) {
-        return (Math.floor(Math.random() * (to - from + 1))) + from;
-    }
+function randomizeIntegerBetween(from, to) {
+    return (Math.floor(Math.random() * (to - from + 1))) + from;
+}
 
 
-    async function queueMessage() {
-        const uuidFraction = UUID().substring(0, 4);
+async function queueMessage() {
+    const uuidFraction = UUID().substring(0, 4);
 
-        const success =
+    const success =
 
-            await producer.send({
-                topic: 'tobechecked',
-                messages: [
-                    {
-                        key: uuidFraction,
-                        value: Buffer.from(idNumbers[randomizeIntegerBetweem(0, idNumbers.length - 1)]),
-                    },
-                ],
-            }
-            );
-
-        if (success) {
-            console.log(`Message ${uuidFraction} succesfully to the stream`);
-        } else {
-            console.log('Problem writing to the stream...');
+        await producer.send({
+            topic: 'tobechecked',
+            messages: [
+                {
+                    key: uuidFraction,
+                    value: Buffer.from(idNumbers[randomizeIntegerBetween(0, idNumbers.length - 1)]),
+                },
+            ],
         }
+        );
+
+    if (success) {
+        console.log(`Message ${uuidFraction} succesfully to the stream`);
+    } else {
+        console.log('Problem writing to the stream...');
     }
+}
